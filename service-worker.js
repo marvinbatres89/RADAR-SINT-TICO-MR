542341e9c14bcb46aa4f9c73ca5af4a09397b85d
marvinbatres89 <marvinbatres17@gmail.com>
2026-09-25 22:29:30 -0600

    Update service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,20 @@
-const CACHE='radar-mr-v190';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE_NAME = 'radar-sintetico-v1.9.0';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://unpkg.com/lightweight-charts@5.0.8/dist/lightweight-charts.standalone.production.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
